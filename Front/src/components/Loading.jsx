7d6a67d321a94cf27d6c013acd80529dd710a0d4
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2024-04-10 08:33:01 -0300

    Novas correcoes

--- Front/src/components/Loading.jsx
+++ Front/src/components/Loading.jsx
@@ -4,7 +4,7 @@ import { ImSpinner8 } from "react-icons/im";
 export default function Loading() {
     return (
         <div className="flex flex-col gap-4 justify-center items-center pt-4 md:pt-20">
-            <label className="text-lg">Quase lá...estou realizando a busca pelos sites parceiros...</label>
+            <label className="text-lg">Quase lá...estou realizando a busca pelos sites...</label>
             <ImSpinner8 className="animate-spin text-4xl" />
         </div>
     );
